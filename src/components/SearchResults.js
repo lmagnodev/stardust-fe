3ef67a54604fbf131f1692e995dbe3e25a682b36
Louise Magno <42506313+lmagnodev@users.noreply.github.com>
2018-08-26 00:57:42 -0700

    adding a single view of image clicked on

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -7,6 +7,7 @@ import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import ListSubheader from '@material-ui/core/ListSubheader';
+import Typography from '@material-ui/core/Typography';
 import Pagination from './Pagination';
 
 const styles = theme => ({
@@ -20,13 +21,60 @@ const styles = theme => ({
   gridList: {
     width: 500,
     height: 450,
+  },
+  singleImage: {
+    width: 500,
+    height: 'auto',
   }
 });
 
 class SearchResults extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedImage: null
+    };
+  }
+
   static propTypes = {
     classes: PropTypes.object.isRequired,
   };
+
+  static getDerivedStateFromProps(props, state) {
+    if (props.isLoading) {
+      return { selectedImage: null };
+    }
+
+    return null;
+  }
+
+  setSelectedImage = (index) => {
+    return () => this.setState({selectedImage: index});
+  }
+
+  renderSingleImage = () => {
+    const { classes, images } = this.props;
+    const selectedImage = images[this.state.selectedImage];
+
+    return (
+      <div className={classes.root}>
+        <div className={classes.singleImage}>
+          <img src={selectedImage.thumbUrl} alt={selectedImage.title} className={classes.gridList}/>
+          <div>
+            <div onClick={ this.setSelectedImage(null) } align="center">[X] Close</div>
+            <Typography variant="headline" gutterBottom>
+              { selectedImage.title }
+            </Typography>
+            <Typography variant="caption" gutterBottom>
+              { selectedImage.description }
+            </Typography>
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   render() {
     const { classes, images } = this.props;
@@ -35,15 +83,19 @@ class SearchResults extends Component {
       return null;
     }
 
+    if (this.state.selectedImage !== null) {
+      return this.renderSingleImage();
+    }
+
     return (
       <div className={classes.root}>
         <GridList cellHeight={180} className={classes.gridList}>
           <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
             <ListSubheader component="div">Images for { this.props.searchStr} ({this.props.resultsCount} results)</ListSubheader>
           </GridListTile>
-          {images.map(image => (
+          {images.map((image, index) => (
             <GridListTile key={image.thumbUrl}>
-              <img src={image.thumbUrl} alt={image.title} />
+              <img src={image.thumbUrl} alt={image.title} onClick={ this.setSelectedImage(index) }/>
               <GridListTileBar
                 title={image.title}
                 subtitle={<span>{image.description}</span>}
@@ -54,7 +106,6 @@ class SearchResults extends Component {
         <div className={classes.gridList}>
           <Pagination />
         </div>
-        
       </div>
     );
   }
